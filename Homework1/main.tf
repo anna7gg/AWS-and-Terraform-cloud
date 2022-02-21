5ed5cdf2280f38c55083cb186b9247466f8ed62a
anna7gg <38318504+anna7gg@users.noreply.github.com>
2022-02-21 21:03:49 +0200

    Update main.tf

--- Homework1/main.tf
+++ Homework1/main.tf
@@ -14,8 +14,23 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+data "aws_ami" "latest-ubuntu" {
+most_recent = true
+owners = ["099720109477"] # Canonical
+
+  filter {
+      name   = "name"
+      values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  filter {
+      name   = "virtualization-type"
+      values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "Whiskey1" {
-  ami           = "ami-04505e74c0741db8d"
+  ami           = "${data.aws_ami.latest-ubuntu.id}"
   instance_type = "t3.micro"
   user_data="${file("install_nginx.sh")}"
   vpc_security_group_ids = ["${aws_security_group.webserver_security_group.id}"]
@@ -26,8 +41,7 @@ resource "aws_instance" "Whiskey1" {
     encrypted             = true
     delete_on_termination = true
   }  
-  
-  
+
   ebs_block_device {
     device_name           = "/dev/sdh"
     volume_size           = "10"
@@ -45,45 +59,19 @@ resource "aws_instance" "Whiskey1" {
 
 
 resource "aws_instance" "Whiskey2" {
-  ami           = "ami-04505e74c0741db8d"
+  ami           = "${data.aws_ami.latest-ubuntu.id}"
   instance_type = "t3.micro"
-user_data     = <<EOT
-#cloud-config
-package_update: true
-packages:
-- nginx
-write_files:
-- content: |
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <title>Grandpa's Whiskey</title>
-      <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-    </head>
-    <body>
-      <p>Welcome to Grandpa's Whiskey</p>
-    </body>
-    </html>
-  path: /var/www/html/index.html
-  permissions: '0644'
-EOT
+  user_data="${file("install_nginx.sh")}"
   vpc_security_group_ids = ["${aws_security_group.webserver_security_group.id}"]
-    
+  availability_zone = "us-east-1f"
+
   root_block_device {
     volume_size           = "10"
     volume_type           = "gp2"
     encrypted             = true
-    delete_on_termination = true
+    delete_on_termination = false
   }  
-  
-  
-  ebs_block_device {
-    device_name           = "/dev/sdh"
-    volume_size           = "10"
-    volume_type           = "gp2"
-    encrypted             = true
-    delete_on_termination = true
-  }
+
   tags = {
     Name = "whiskyTwo"
     owner= "Grandpa"
@@ -91,3 +79,14 @@ EOT
   }  
 }
 
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.non_root.id
+  instance_id = aws_instance.Whiskey2.id
+}
+
+resource "aws_ebs_volume" "non_root" {
+  availability_zone = "us-east-1f"
+  size = 10
+  encrypted = true
+}
